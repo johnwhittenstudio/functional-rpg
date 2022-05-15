3c944773050f9d31d29da8382fd1964c47892779
John Whitten <johnwhitten.studio@gmail.com>
2022-05-15 14:39:27 -0700

    test passing should return state health of 10

--- __tests__/student.test.js
+++ __tests__/student.test.js
@@ -1,7 +1,18 @@
 import { storeState, changeState } from './../src/js/student';
 
 
-const stateControl = storeState(); 
+// const stateControl = storeState(); 
+let stateControl
+let jock
+beforeEach(() => {
+  jock = {
+    health: 10,
+    brain:1,
+    charm: 1,
+    braun: 2
+  };
+  stateControl = storeState(jock);
+});
 
 describe('storeState', () => {
   test('should create a state', () => {
@@ -9,4 +20,16 @@ describe('storeState', () => {
   });
 });
 
+describe('changeState', () => {
+  test('should return default state health of 10', () => {
+    const resultState = stateControl();
+    expect(resultState.health).toEqual(10);
+  });
+  test('should set state health to 10', () => {
+    const addHealth = changeState("health")(10);
+    const newState = stateControl(addHealth);
+    const resultState = stateControl();
+    expect(resultState.health).toEqual(20);
+  });
+});
 
